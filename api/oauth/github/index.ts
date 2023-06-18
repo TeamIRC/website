1c9ea174c689be74f9e1111d8adafdcbb0ad5436
Djaymare <djayp34@gmail.com>
2023-06-18 10:36:24 +0200

    fix header

--- api/oauth/github/index.ts
+++ api/oauth/github/index.ts
@@ -1,31 +1,21 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-    let req = await fetch('https://github.com/login/oauth/access_token', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            client_id: process.env.VITE_GITHUB_CLIENT_ID,
-            client_secret: process.env.GITHUB_CLIENT_SECRET,
-            code: request.body
-        }),
-    });
-    const json = await req.json();
-    console.log(json);   
-    req = await fetch('https://api.github.com/user', {
-        headers: {
-            Authorization: `bearer ${json.access_token}`
-        }
-    })
-    console.log(await req.text());
-    const user = await req.json();
-    console.log(user);
-
+    const { access_token } = await (
+        await fetch('https://github.com/login/oauth/access_token', {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                client_id: process.env.VITE_GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code: request.body
+            }),
+    })).json();
     response
-        .setHeader('Set-Cookie', `github_token=${JSON.stringify(json.access_token)}; Path=/; Secure; HttpOnly`)
-        .send(user);
+        .setHeader('Set-Cookie', `github_token=${JSON.stringify(access_token)}; Path=/; Secure; HttpOnly`)
+        .send(await getGithubUser(access_token));
 }
 
 async function getGithubUser (access_token: string) {
