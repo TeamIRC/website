4d3d6e0cf9e4f04e9d068db6b6ec1b28cf01f83b
Djaymare <djayp34@gmail.com>
2023-06-23 18:36:54 +0200

    avoid clear query

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,6 @@ import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 
 const params = new URL(document.location.href).searchParams;
-document.location.search = "";
 const router = useRouter();
 const previous = localStorage.getItem('origin')!;
 
