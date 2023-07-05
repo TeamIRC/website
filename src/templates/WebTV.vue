4f92b17b2b23f4e71a20f56d188973f047199c5e
Djaymare <djayp34@gmail.com>
2023-07-05 17:48:42 +0200

    fill twitch profile

--- src/templates/WebTV.vue
+++ src/templates/WebTV.vue
@@ -26,8 +26,17 @@ watch(() => props.edit,
 				Chargement
 			</template>
 			<TwitchClient :content="content" v-slot="{profiles}">
-				<div v-for="profile in profiles">
-					{{profile.stream?.thumbnail_url}}
+				<div v-for="{user, stream } in profiles">
+					<img :src='user.profile_image_url' />
+					<h2>{{ user.display_name }}</h2>
+					<p>{{ user.description }}</p>
+					<div v-if="stream">
+						<img :src='stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080")' />
+						<h3>{{ stream.title }}</h3>
+						<p>{{ stream.viewer_count }} viewers</p>
+						<p>Joue à {{ stream.game_name }}</p>
+						<p>Stream depuis {{ stream.started_at }}</p>
+					</div>
 				</div>
 			</TwitchClient>
 		</Suspense>
