f7e5f0d419030c1964893f6fa59085f709222785
Djaymare <djayp34@gmail.com>
2023-07-13 14:09:04 +0200

    try fix importAssertions

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { importAssertions } from 'acorn-import-assertions';
-import { importAssertionsPlugin } from 'rollup-plugin-import-assert';
 import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { rollupOptions: {
-    acornInjectPlugins: [importAssertions], 
-    plugins: [ importAssertionsPlugin() ]
-  }},
   plugins: [vue(), svgLoader()],
 })
