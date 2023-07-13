2230f0264803e6cf04eef323f2e0ef581c41f58b
Djaymare <djayp34@gmail.com>
2023-07-13 16:08:07 +0200

    test stream page

--- src/templates/WebTV.vue
+++ src/templates/WebTV.vue
@@ -26,16 +26,19 @@ watch(() => props.edit,
 				Chargement
 			</template>
 			<TwitchClient :content="content" v-slot="{profiles}">
-				<div v-for="{user, stream } in profiles">
-					<img :src='user.profile_image_url' />
-					<h2>{{ user.display_name }}</h2>
-					<p>{{ user.description }}</p>
-					<div v-if="stream">
-						<img :src='stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080")' />
-						<h3>{{ stream.title }}</h3>
-						<p>{{ stream.viewer_count }} viewers</p>
-						<p>Joue à {{ stream.game_name }}</p>
-						<p>Stream depuis {{ stream.started_at }}</p>
+				<div id="embed"></div>
+				<div id="profiles">
+					<div class="card" v-for="{ user, stream } in profiles">
+						<img :src='user.profile_image_url' />
+						<h2>{{ user.display_name }}</h2>
+						<p>{{ user.description }}</p>
+						<div v-if="stream">
+							<img :src='stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080")' />
+							<h3>{{ stream.title }}</h3>
+							<p>{{ stream.viewer_count }} viewers</p>
+							<p>Joue à {{ stream.game_name }}</p>
+							<p>Stream depuis {{ stream.started_at }}</p>
+						</div>
 					</div>
 				</div>
 			</TwitchClient>
@@ -43,4 +46,22 @@ watch(() => props.edit,
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#embed {
+	width: 100%;
+	aspect-ratio: 16 / 9;
+	background-color: #00000033;
+}
+
+#profiles {
+	display: grid;
+	grid-auto-columns: minmax(344px, auto);
+	gap: 16px;
+}
+
+.card {
+	border: 1px solid var(--secondary-lt-2);
+	border-radius: 16px;
+	background-color: var(--secondary-dk-2);
+}
+</style>
