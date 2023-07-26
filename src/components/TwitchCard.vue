1065a80e99f995eb480280ae19be404446f8b272
Djaymare <djayp34@gmail.com>
2023-07-26 14:38:51 +0200

    fix teleport 2

---
+++ src/components/TwitchCard.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { TwitchStream, TwitchUser } from '../types';
+
+defineProps<{
+    user: TwitchUser;
+    stream: TwitchStream | undefined;
+}>()
+</script>
+
+<template>
+    <div class="card">
+        <img :src='user.profile_image_url' />
+        <h2>{{ user.display_name }}</h2>
+        <p>{{ user.description }}</p>
+        <Teleport to="#streams">
+            <div class="stream" v-if="stream">
+                <img :src='stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080")' />
+                <h3>{{ stream.title }}</h3>
+                <p>{{ stream.viewer_count }} viewers</p>
+                <p>Joue à {{ stream.game_name }}</p>
+                <p>Stream depuis {{ stream.started_at }}</p>
+            </div>
+        </Teleport>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    text-align: center;
+	font-weight: bold;
+	background-color: var(--secondary-dk-2);
+	border: 1px solid var(--secondary-lt-2);
+	border-radius: 16px;
+    padding: 16px;
+}
+
+.card img {
+    width: 100%;
+}
+
+.card > img {
+	border-radius: 50%;
+}
+
+.stream {
+	font-weight: unset;
+	background-color: var(--secondary-dk-3);
+	border: 1px solid var(--secondary-lt-3);
+	border-radius: 16px;
+    padding: 16px;
+}
+</style>
--- src/templates/WebTV.vue
+++ src/templates/WebTV.vue
@@ -4,6 +4,7 @@ import TwitchClient from '../components/TwitchClient.vue';
 import { ListContent } from '../types';
 import TwitchEmbed from '../components/TwitchEmbed.vue';
 import ListBase from '../components/ListBase.vue';
+import TwitchCard from '../components/TwitchCard.vue';
 const props = defineProps<{
 	content: ListContent<string>,
 	edit: boolean
@@ -47,20 +48,10 @@ watch(
 				<div id="streams">
 				</div>
 				<div id="profiles">
-					<div class="card" v-for="{ user, stream } in profiles">
-						<img :src='user.profile_image_url' />
-						<h2>{{ user.display_name }}</h2>
-						<p>{{ user.description }}</p>
-						<Teleport to="#streams">
-							<div class="stream" v-if="stream">
-								<img :src='stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080")' />
-								<h3>{{ stream.title }}</h3>
-								<p>{{ stream.viewer_count }} viewers</p>
-								<p>Joue à {{ stream.game_name }}</p>
-								<p>Stream depuis {{ stream.started_at }}</p>
-							</div>
-						</Teleport>
-					</div>
+					<TwitchCard
+						v-for="{ user, stream } in profiles"
+						:user="user"
+						:stream="stream" />
 				</div>
 			</TwitchClient>
 		</Suspense>
@@ -78,30 +69,5 @@ watch(
 	display: grid;
     grid-template-columns: repeat(auto-fit, minmax(344px, auto));
 	gap: 16px;
-}
-
-.card {
-    text-align: center;
-	font-weight: bold;
-	background-color: var(--secondary-dk-2);
-	border: 1px solid var(--secondary-lt-2);
-	border-radius: 16px;
-    padding: 16px;
-}
-
-.card img {
-    width: 100%;
-}
-
-.card > img {
-	border-radius: 50%;
-}
-
-.stream {
-	font-weight: unset;
-	background-color: var(--secondary-dk-3);
-	border: 1px solid var(--secondary-lt-3);
-	border-radius: 16px;
-    padding: 16px;
 }
 </style>
