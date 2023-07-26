3dec9cf473aef39a564c25305c4692941016b458
Djaymare <djayp34@gmail.com>
2023-07-26 15:35:22 +0200

    fix webtv style

--- src/components/TwitchCard.vue
+++ src/components/TwitchCard.vue
@@ -17,26 +17,35 @@ defineProps<{
             </div>
             <div class="stream" v-if="stream">
                 <img :src='stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080")' />
-                <h3>{{ stream.title }}</h3>
-                <p>{{ stream.viewer_count }} viewers</p>
-                <p>Joue à {{ stream.game_name }}</p>
-                <p>Stream depuis {{ stream.started_at }}</p>
+                <div>
+                    <h3>{{ stream.title }}</h3>
+                    <p>{{ stream.viewer_count }} viewers</p>
+                    <p>Joue à {{ stream.game_name }}</p>
+                    <p>Stream depuis {{ stream.started_at }}</p>
+                </div>
             </div>
         </div>
     </Teleport>
 </template>
 
 <style scoped>
-#streams > .card {
-    display: flex;
-    gap: 16px;
-}
-
 #streams > .card > .user {
+    display: flex;
     width: 33%;
 }
 
+#streams > .card > .user > p {
+    display: none;
+}
+
+#streams > .card > .user > img {
+    width: 64px;
+    height: 64px;
+}
+
 .card {
+    display: flex;
+    gap: 16px;
     text-align: center;
 	font-weight: bold;
 	background-color: var(--secondary-dk-2);
@@ -45,19 +54,18 @@ defineProps<{
     padding: 16px;
 }
 
-.card img {
+.card > img {
+	border-radius: 50%;
     width: 100%;
 }
 
-.card > img {
-	border-radius: 50%;
+.stream {
+    display: flex;
+	font-weight: unset;
 }
 
-.stream {
-	font-weight: unset;
-	background-color: var(--secondary-dk-3);
-	border: 1px solid var(--secondary-lt-3);
-	border-radius: 16px;
-    padding: 16px;
+.stream > img {
+    width: 256px;
+    height: 144px;
 }
 </style>
