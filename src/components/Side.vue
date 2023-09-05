eebdf8e448e1c718b8c0fb9d1ddc021115029a7d
Djaymare <djayp34@gmail.com>
2023-09-05 18:11:39 +0200

    try fix webtv routing

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -53,7 +53,7 @@ onMounted(() => setTitle());
                     :title="routes.find(v => v.path == $route.path.split('/')[2])?.title"
                     >
                     <template v-if="Component">
-                        <KeepAlive>
+
                             <Suspense>
                                 <Transition name="fade" mode="out-in">
                                     <template #default>
@@ -64,7 +64,7 @@ onMounted(() => setTitle());
                                     </template>
                                 </Transition>
                             </Suspense>
-                        </KeepAlive>
+
                     </template>
                 </router-view>
             </main>
