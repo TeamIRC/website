89714b31515c742bc774040dc7bdd73232f87b40
Djaymare <djayp34@gmail.com>
2025-02-25 15:42:41 +0100

    Test @types/node v18

--- backend/vercel/github/client.ts
+++ backend/vercel/github/client.ts
@@ -1,21 +1,21 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { parse } from "cookie";
+// import { VercelRequest, VercelResponse } from "@vercel/node";
+// import { parse } from "cookie";
 
-export function client(fn: (req: VercelRequest, res: VercelResponse, token: string) => Promise<void>) {
-    return async (request: VercelRequest, response: VercelResponse) => {
-        if (!request.headers.cookie) {
-            response.status(501).end();
-            return;
-        }
-        const cookie = parse(request.headers.cookie)
-        let token = cookie.github_token;
-    /*    if (!token) {
+// export function client(fn: (req: VercelRequest, res: VercelResponse, token: string) => Promise<void>) {
+//     return async (request: VercelRequest, response: VercelResponse) => {
+//         if (!request.headers.cookie) {
+//             response.status(501).end();
+//             return;
+//         }
+//         const cookie = parse(request.headers.cookie)
+//         let token = cookie.github_token;
+//     /*    if (!token) {
             
-        }    */     
-        return await fn(
-            request,
-            response,
-            token
-        )
-    }
-}
+//         }    */     
+//         return await fn(
+//             request,
+//             response,
+//             token
+//         )
+//     }
+// }
